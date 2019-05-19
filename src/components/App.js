be094549a2b16af85395d1cdd0e3aaaf2cfce7b4
sam80719 <sam80719@gmail.com>
2019-05-19 21:13:17 +0800

    ffinal test YT API

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
 
 	state = { videos: [], selectedVideo: null };
+
+	componentDidMount(){
+		this.onTermSubmit('');
+	}
 
 	onTermSubmit = async term => {
 		// console.log(term);
@@ -18,21 +23,34 @@ class App extends React.Component {
 		});
 
 		// console.log(response);
-		this.setState({ videos: response.data.items });
+		this.setState({ 
+			videos: response.data.items,
+			selectedVideo: response.data.items[0]
+		});
 
 	};
 
 	onVideoSelect = video => {
 		// console.log('From the App!!', video);
-		
+		this.setState({ selectedVideo: video });
+
 	};
 
 	render() {
 		return (
 			<div>
 				<SearchBar onFormSubmit={this.onTermSubmit} />
+				<div className='ui row'>
+					<div className='ui row'>
+						<div className='eleven wide column'>
+							<VideoDetail video={this.state.selectedVideo} />
+						</div>
+						<div className='five wide column'>
+							<VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+						</div>
+					</div>
+				</div>
 
-				<VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
 			</div>
 
 		);
